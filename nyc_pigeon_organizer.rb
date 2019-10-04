002f9d401f84e151d48ffd5e83a5b56be00a8857
Brad Farberman <bfarberman@gmail.com>
2019-10-04 00:51:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
   pigeon_data.each do |property, property_hash|
-    property_hash.each do |values, |
+    property_hash.each do |values, pigeon_array|
+      
   
