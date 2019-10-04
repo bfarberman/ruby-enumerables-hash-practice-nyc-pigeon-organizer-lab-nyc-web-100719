a46ba6e6f2ed10e11bf696e73db23c8c140be322
Brad Farberman <bfarberman@gmail.com>
2019-10-04 00:50:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,31 +1,3 @@
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
-  pigeon_data.each do |element, hash_element|
-    hash_element.each do |array_of_pigeons, values|
-      array_of_pigeons.each do |name|
-        if pigeon_list[name]
-          if pigeon_list[name][element]
-            pigeon_list[name][element] << values.to_s 
-          else 
-            pigeon_list[name][element] = [values.to_s]
-        end
-      else 
-        pigeon_list[name] = {element => [values.to_s]}
-    end
-  end
-end
-end
-pigeon_list
-end 
-
-
-
-
-
-
-
-
-
-
-
-
+  
